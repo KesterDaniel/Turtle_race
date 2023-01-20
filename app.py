1fca27c13ff4ee985e104461983357ad5ada593f
KesterDaniel <kesterdan17@gmail.com>
2023-01-20 16:01:55 +0100

    getting the turtle to move to the starting point

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 # Turtle race
 from turtle import Turtle, Screen
 
-timmy = Turtle()
 screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Place Bet", prompt="Which turtle win win the race?")
 print(user_bet)
+
+timmy = Turtle(shape="turtle")
+timmy.penup()
+
+timmy.goto(-240, -100)
 
 
 screen.exitonclick()
