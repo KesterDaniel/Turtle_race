b91d7b608b3ec438d62d69568cf3c7be420dfacd
KesterDaniel <kesterdan17@gmail.com>
2023-01-20 12:09:27 +0100

    setting the screen size and collecting user input

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
 # Turtle race
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Place Bet", prompt="Which turtle win win the race?")
+print(user_bet)
+
+
+screen.exitonclick()
