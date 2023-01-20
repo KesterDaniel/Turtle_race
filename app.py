d04c695fef90575ccb8c5d007a7dd71cc135d9d8
KesterDaniel <kesterdan17@gmail.com>
2023-01-20 17:46:25 +0100

    created multiple turtles and moved them to the starting point

--- app.py
+++ app.py
@@ -4,12 +4,22 @@ from turtle import Turtle, Screen
 screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Place Bet", prompt="Which turtle win win the race?")
-print(user_bet)
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 
-timmy = Turtle(shape="turtle")
-timmy.penup()
+turtles = []
 
-timmy.goto(-240, -100)
+for color in colors:
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(color)
+    turtles.append(new_turtle)
+
+y_position = -100
+
+for turtle in turtles:
+    turtle.penup()
+    turtle.goto(-240, y_position)
+    y_position += 30
+
 
 
 screen.exitonclick()
